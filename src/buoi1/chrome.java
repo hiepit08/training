d9f0d47a9d1f21b094a52e7fb41ab1aa3d112cd9
hieppt <hieppt@hieppt.officevnpay.com>
2019-11-14 09:35:55 +0700

    Thuc hanh buoi 2

--- src/buoi1/chrome.java
+++ src/buoi1/chrome.java
@@ -1,28 +1,63 @@
 package buoi1;
 
 import org.testng.annotations.Test;
+
+import pageObjects.HomePageObject;
+
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.AfterClass;
+
+import static org.junit.Assert.assertEquals;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-
 public class chrome {
- 
-  @BeforeClass
-  public void beforeClass() {
-	  WebDriver driver;
-	  System.setProperty("webdriver.chrome.driver", "D:\\setup\\chromedriver.exe");
-  driver =new ChromeDriver();
-	  driver.get("https://www.google.com/search?q=google&rlz=1C1GCEU_viVN873VN873&oq=go&aqs=chrome.1.69i57j0l4j69i60.2624j0j7&sourceid=chrome&ie=UTF-8");
-  }
-  @Test
-  public void f() {
-	  System.out.print("hiepPT");
-	  
-  }
-  @AfterClass
-  public void afterClass() {
-  }
+	WebDriver driver;
+
+	@BeforeClass
+	public void beforeClass() {
+//buoi 1
+		System.setProperty("webdriver.chrome.driver", "D:\\setup\\chromedriver.exe");
+		driver = new ChromeDriver();
+
+	}
+
+	// buoi 2
+	@Test
+	public void TC_01_empty() {
+
+		driver.get("http://live.demoguru99.com/");
+		driver.findElement(By.xpath("//div[@class='footer']//a[text()='My Account']")).click();
+		driver.findElement(By.xpath("//span[text()='Login']")).click();
+		String error = driver.findElement(By.xpath("//div[@id=\"advice-required-entry-email\"]")).getText();
+		assertEquals("This is a required field.", error);
+
+	}
+	@Test
+	public void TC_02_incorrectEmail() {
+		driver.findElement(By.xpath("//input[@name=\"login[username]\"]")).sendKeys("qwerr123");
+		driver.findElement(By.xpath("//input[@name=\"login[password]\"]")).sendKeys("abc@123");
+		driver.findElement(By.xpath("//span[text()='Login']")).click();
+		String error = driver.findElement(By.xpath("//div[@id=\"advice-required-entry-email\"]")).getText();
+		assertEquals("This is a required field.", error);
+
+	}
+	@Test
+	public void TC_03_incorrectPass() {
+		driver.findElement(By.xpath("//input[@id=\"email\"]")).clear();
+		driver.findElement(By.xpath("//input[@name=\"login[password]\"]")).clear();
+		driver.findElement(By.xpath("//input[@id=\"email\"]")).sendKeys("hiepit08@gmail.com");
+		driver.findElement(By.xpath("//input[@name=\"login[password]\"]")).sendKeys("abc");
+		driver.findElement(By.xpath("//span[text()='Login']")).click();
+		String error = driver.findElement(By.xpath("//div[@id=\"advice-validate-password-pass\"]")).getText();
+		assertEquals("Please enter 6 or more characters without leading or trailing spaces.", error);
+
+	}
+
+	@AfterClass
+	public void afterClass() {
+	}
 
 }
